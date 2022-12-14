d53dcf3fd8122d9500c3b30e6892da82e30f5d22
Stephan Pitois <spitois@gmail.com>
2022-12-13 22:52:53 -0500

    UI tweaks

--- src/NoFace.svelte
+++ src/NoFace.svelte
@@ -3,11 +3,16 @@
 <script>
 </script>
 
-<main part="mfeMain">
-	<h1 part="mfeMainH1">MFE 3: No-Face (顔無し, Kaonashi)</h1>
-	<img
-		part="mfeMainImg"
-		src="/img/no-face.jpeg"
-		alt="No-Face from Spirited Away"
-	/>
-</main>
+<div part="mfeCard">
+	<div part="mfeCardTitle">Micro-Frontend</div>
+	<div part="mfeCardBody">
+		<main part="mfeMain">
+			<h1 part="mfeMainH1">MFE 3: No-Face (顔無し, Kaonashi)</h1>
+			<img
+				part="mfeMainImg"
+				src="/img/no-face.jpeg"
+				alt="No-Face from Spirited Away"
+			/>
+		</main>
+	</div>
+</div>
