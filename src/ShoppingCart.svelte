9e13b797cc4ad430520751b239d201cc5da4c034
Stephan Pitois <spitois@gmail.com>
2023-02-07 00:16:30 -0500

    Cart icon

--- src/ShoppingCart.svelte
+++ src/ShoppingCart.svelte
@@ -23,7 +23,12 @@
     ]);
 </script>
 
-<div class="cartContent">
+<div class="cartContent flex-row gap-5">
+    <img
+        style="width:24px; height: 24px"
+        alt="GitHub"
+        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9IiM4ZTI0YWEiIGQ9Ik0xNyAxOGMtMS4xMSAwLTIgLjg5LTIgMmEyIDIgMCAwIDAgMiAyYTIgMiAwIDAgMCAyLTJhMiAyIDAgMCAwLTItMk0xIDJ2MmgybDMuNiA3LjU5bC0xLjM2IDIuNDVjLS4xNS4yOC0uMjQuNjEtLjI0Ljk2YTIgMiAwIDAgMCAyIDJoMTJ2LTJINy40MmEuMjUuMjUgMCAwIDEtLjI1LS4yNWMwLS4wNS4wMS0uMDkuMDMtLjEyTDguMSAxM2g3LjQ1Yy43NSAwIDEuNDEtLjQyIDEuNzUtMS4wM2wzLjU4LTYuNDdjLjA3LS4xNi4xMi0uMzMuMTItLjVhMSAxIDAgMCAwLTEtMUg1LjIxbC0uOTQtMk03IDE4Yy0xLjExIDAtMiAuODktMiAyYTIgMiAwIDAgMCAyIDJhMiAyIDAgMCAwIDItMmEyIDIgMCAwIDAtMi0yWiIvPjwvc3ZnPg=="
+    />
     {#if $cart.length}
         <a href="/shopping-cart">{itemCount} item(s) in shopping cart </a>
     {:else}
@@ -33,6 +38,6 @@
 
 <style>
     .cartContent {
-        padding: 5px 10px;
+        /* padding: 5px 10px; */
     }
 </style>
